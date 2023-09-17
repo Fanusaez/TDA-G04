167d6de794ad848bfbcd8af26863638668837fb5
Alexis Herrera <alexysaguilar@gmail.com>
2023-09-17 16:44:52 -0300

    agrego test

--- test_ejemplos.py
+++ test_ejemplos.py
@@ -8,34 +8,32 @@ def test_caso_con_solo_1_partido():
     assert obtener_tiempo_minimo(s, a) == 2
     
 def test_caso_con_2_partidos():
-    # S  # 1 2 3 4 5 6 7 8 9
-    # A1 # s s
-    # A2 # - a a a
-    # A3 # - - a a 
     s = [1, 1]
     a = [2, 3]
     assert obtener_tiempo_minimo(s, a) == 4
 
-def test_caso_1_ejemplo_catedra_3_elem():
-    # Dado el path: textos_pruebas/3_elem.txt
+def test_ejemplo_1_tp():
+    s = [60, 50, 70]
+    a = [20, 10, 5]
+    assert obtener_tiempo_minimo(s, a) == 185
 
+def test_ejemplo_2_tp():
+    s = [20, 10, 5]
+    a = [60, 50, 70]
+    assert obtener_tiempo_minimo(s, a) == 85
+
+def test_ejemplo_catedra_3_elem():
     s, a = obtener_tiempos_de_archivo('./textos_pruebas/3_elem.txt')
     assert obtener_tiempo_minimo(s, a) == 10
 
-def test_caso_2_ejemplo_catedra_10_elem():
-    # Dado el path: textos_pruebas/10_elem.txt
-
+def test_ejemplo_catedra_10_elem():
     s, a = obtener_tiempos_de_archivo('./textos_pruebas/10_elem.txt')
     assert obtener_tiempo_minimo(s, a) == 29
 
-def test_caso_3_ejemplo_catedra_100_elem():
-    # Dado el path: textos_pruebas/100_elem.txt
-
+def test_ejemplo_catedra_100_elem():
     s, a = obtener_tiempos_de_archivo('./textos_pruebas/100_elem.txt')
     assert obtener_tiempo_minimo(s, a) == 5223
 
-def test_caso_4_ejemplo_catedra_10000_elem():
-    # Dado el path: textos_pruebas/10000_elem.txt
-
+def test_ejemplo_catedra_10000_elem():
     s, a = obtener_tiempos_de_archivo('./textos_pruebas/10000_elem.txt')
     assert obtener_tiempo_minimo(s, a) == 497886735
