4c3f5a553f7ce8342943df27d42ca017ed19c4d3
fanusaez <esaez@fi.uba.ar>
2023-11-12 18:36:27 -0300

    Modifico hitting_set para devolver la solucion optima con cualquier K. Tambien agrego algunos tests.

--- TP3/hitting_set.py
+++ TP3/hitting_set.py
@@ -18,41 +18,26 @@ def _hitting_set(universo, subconjuntos, k, indice_elemento, asignacion_actual):
         return []
 
     if es_solucion(subconjuntos, asignacion_actual):
-        return asignacion_actual
+        return asignacion_actual[:]
 
     if indice_elemento >= len(universo):
         return []
 
+    mejor_asignacion = []
+
     asignacion_actual.append(universo[indice_elemento])
     resultado = _hitting_set(universo, subconjuntos, k, indice_elemento + 1, asignacion_actual)
-
-    if len(resultado) > 0:
-        return resultado
+    if resultado:
+        mejor_asignacion = resultado
 
     asignacion_actual.pop()
+    resultado = _hitting_set(universo, subconjuntos, k, indice_elemento + 1, asignacion_actual)
+    if resultado:
+        if not mejor_asignacion or len(resultado) < len(mejor_asignacion):
+            mejor_asignacion = resultado
 
-    return _hitting_set(universo, subconjuntos, k, indice_elemento + 1, asignacion_actual)
-
+    return mejor_asignacion
 
 def hitting_set(universo, subconjuntos, k):
     return _hitting_set(universo, subconjuntos, k, 0, [])
-
-
-def main():
-    """conjunto_universo = ["A", "B", "C", "D", "F"]
-    subconjuntos = [["A","B"], ["B", "C"], ["B","D"], ["D", "F"]]
-    k = 5
-
-    print(hitting_set(conjunto_universo, subconjuntos, k))
-    """
-    universo = parsear_archivo("TP3/20.txt")[0]
-    subconjuntos = parsear_archivo("TP3/20.txt")[1]
-    print(universo)
-    print(subconjuntos)
-    k = 5
-    print(hitting_set(universo, subconjuntos, k))
-
-
-if __name__ == '__main__':
-    main()
 
--- TP3/test_catedra.py
+++ TP3/test_catedra.py
@@ -1,1 +1,48 @@
-# def test_ejemplo_1():
+from hitting_set import hitting_set
+from parser_casos import parsear_archivo
+
+
+def test_ejemplo_5():
+    universo, subconjunto = parsear_archivo('TP3/5.txt')
+    k = 2
+    assert hitting_set(universo, subconjunto, k) == ["Barcon't", "Messi"]
+
+
+def test_ejemplo_7():
+    universo, subconjunto = parsear_archivo('TP3/7.txt')
+    k = 2
+    assert hitting_set(universo, subconjunto, k) == ["Mauro Zarate", "Pezzella"]
+
+
+def test_ejemplo_10_pocos():
+    universo, subconjunto = parsear_archivo('TP3/10_pocos.txt')
+    k = 3
+    assert hitting_set(universo, subconjunto, k) == ["Chiquito Romero", "Di Maria", "Casco"]
+
+
+def test_ejemplo_10_todos():
+    universo, subconjunto = parsear_archivo('TP3/10_todos.txt')
+    k = 10
+    assert hitting_set(universo, subconjunto, k) == ['Dibu', 'Cuti', 'Molina', 'Guido Rodriguez', 'Paredes',
+                                                     'Palacios', 'Messi', 'Garnacho', 'Lautaro', 'Perrone']
+
+def test_ejemplo_10_varios():
+    universo, subconjunto = parsear_archivo('TP3/10_varios.txt')
+    k = 6
+    assert hitting_set(universo, subconjunto, k) == ['Palermo', 'Dibu', 'Beltran', 'Riquelme', 'Dybala', 'Di Maria']
+
+def test_ejemplo_15():
+    universo, subconjunto = parsear_archivo('TP3/15.txt')
+    k = 4
+    assert hitting_set(universo, subconjunto, k) == ['Chiquito Romero', 'Palermo', 'Luka Romero', 'Dybala']
+
+def test_ejemplo_20():
+    universo, subconjunto = parsear_archivo('TP3/20.txt')
+    k = 5
+    assert hitting_set(universo, subconjunto, k) == ["Barcon't", 'Riquelme', 'El fantasma de la B',
+                                                     'Mauro Zarate', 'Ogro Fabianni']
+
+def test_ejemplo_50():
+    universo, subconjunto = parsear_archivo('TP3/50.txt')
+    k = 10
+    assert hitting_set(universo, subconjunto, k) == ["Casco", "Barcon't", "Tucu Pereyra", "Dybala", "Armani", "Langoni"]
