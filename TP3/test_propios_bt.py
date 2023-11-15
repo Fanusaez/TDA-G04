80c7a008a7aa9c8f90794dfd1a14db681fdc52dc
fanusaez <esaez@fi.uba.ar>
2023-11-15 14:26:07 -0300

    Agrego mas tests a mano

--- TP3/test_propios_bt.py
+++ TP3/test_propios_bt.py
@@ -10,3 +10,22 @@ def test_ejemplo_5_unico():
 def test_ejemplo_5_todos():
     universo, subconjunto = parsear_archivo('sets_generados/5_todos.txt')
     assert hitting_set(universo, subconjunto) == ["Barcon't", "Colo Barco", "Wachoffisde Abila", "Messi", "Cuti Romero"]
+
+
+def test_ejemplo_10_unico():
+    universo, subconjunto = parsear_archivo('sets_generados/10_unico.txt')
+    assert hitting_set(universo, subconjunto) == ["Dibu"]
+
+
+def test_ejemplo_10_pocos():
+    universo, subconjunto = parsear_archivo('sets_generados/10_pocos.txt')
+    assert hitting_set(universo, subconjunto) == ["Messi", "Di Maria"]
+
+
+def test_ejemplo_10_varios():
+    universo, subconjunto = parsear_archivo('sets_generados/10_varios.txt')
+    assert hitting_set(universo, subconjunto) == ['Di Maria', 'Colo Barco', 'Tagliafico', 'Martinez', 'Alvarez', 'Perez']
+
+def test_ejemplo_15():
+    universo, subconjunto = parsear_archivo('sets_generados/15.txt')
+    assert hitting_set(universo, subconjunto) == ["Alvarez", "Fernandez",  "Dibu"]
