71625f3eecadc5f6a8c17e31fd69ff2f71ce7b07
fanusaez <esaez@fi.uba.ar>
2023-11-24 17:26:51 -0300

    main

---
+++ TP3/main.py
@@ -0,0 +1,9 @@
+import sys
+from parser_casos import parsear_archivo
+from hitting_set import hitting_set as hitting_set_bt
+from programacion_lineal import hitting_set as hitting_set_pl
+from programacion_lineal import hitting_set_aproximado as hitting_set_plc
+from hitting_set_greedy import hitting_set_greedy
+
+
+if __name__ == '__main__':
